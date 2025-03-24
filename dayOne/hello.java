0e70160f95251f0a609e4ed6def49578cec8c2e2
Loug <lucianoguasque@gmail.com>
2025-03-24 20:24:28 -0300

    day two

--- dayOne/hello.java
+++ dayOne/hello.java
@@ -1,14 +1,16 @@
 class Hello
 {
-    public static void main(String a[]) 
+    public static void main(String[] args) 
     {
-        /* byte  b = 127; */
+        System.err.println("Hello, world!");
+
+        /* byte  b = 127;
         int c = 257;
         byte k = (byte) c; // performs a modulus operation, resulting in 1
         System.out.println(k);
 
         float f = 5.6f;
         int t = (int) f;
-        System.out.println(t);
+        System.out.println(t); */
     }
 }
---
+++ dayTwo/operators.java
@@ -0,0 +1,19 @@
+class Operators
+{
+    public static void main(String[] args)
+    {
+        int x = 1;
+        while (x <= 7) 
+        {
+            System.out.println(x);
+            x++;
+        }
+
+        /* int x = 6;
+        int y = 7;
+        int res = x>y ? x : y;
+        System.out.println(res);
+        boolean result = x < y;
+        System.err.println(result ? 'x' : 'y');*/ 
+    }
+}
