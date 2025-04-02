acac5712f95a55b6c35aa734ee397eaae1b1af06
Loug <lucianoguasque@gmail.com>
2025-04-01 22:47:47 -0300

    formatted some code and added dayEight

--- daySeven/Encapsulation.java
+++ daySeven/Encapsulation.java
@@ -5,26 +5,29 @@ public class Encapsulation
     public static void main(String[] args) 
     {
         Human obj = new Human();
-        obj.name = "Loug";
-        obj.setAge(30);
+        obj.setName("Loug");
+        obj.setAge(32);
 
-        System.out.println(obj.name + " is " + obj.getAge() + " years old.");
+        System.out.println(obj.getName() + " is " + obj.getAge() + " years old.");
     }    
 }
 
 
 class Human
 {
-    private int age = 32;
+    private int age;
     String name;
 
-    public int getAge()
-    {
+    public int getAge() {
         return age;
     }
-
-    public void setAge(int a)
-    {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
